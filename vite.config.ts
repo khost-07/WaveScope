5017745d91852bf43b8a18bc9363cbe484f15970
Khost-07 <kaushikrammadhavan@gmail.com>
2026-08-18 03:27:38 +0530

    fix(vite): explicitly disable postcss plugins in vite.config.ts and uninstall tailwind packages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: []
+    }
+  },
   server: {
     port: 5173,
     host: true
